6137bd8ff04465e3b782d79ba1399a4b714054d8
zaidrawashdeh911 <zai20200907@std.psut.edu.jo>
2025-03-20 04:47:53 +0300

    Create a custom hook to fetch the games

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface Game{
+    id:number;
+    name:string;
+}
+
+interface FetchGamesResponse{
+    count: number;
+    results: Game[]
+}
+
+const useGames = ()=>{
+    const [games, setGames] = useState<Game[]>([]);
+    const [error, setError] = useState('');
+
+    //to set a fetch request to the backend
+    useEffect(()=>{
+        const controller = new AbortController();
+
+        apiClient.get<FetchGamesResponse>('/games', {signal: controller.signal})
+        .then(res=> setGames(res.data.results))
+        .catch(err=>{
+            if(err instanceof CanceledError) return;
+            setError(err.message)});
+
+        return ()=> controller.abort();
+    }, []);// the empty array this, is the array of dependencies, without it we can't send a request to the backend
+
+    return {games, error};
+}
+
+export default useGames;
