51ff0e47e391942b948064391e2c295d5576ad60
zaidrawashdeh911 <zai20200907@std.psut.edu.jo>
2025-03-20 06:23:16 +0300

    Display critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import { Game } from '../hooks/useGames'
-import { Card, CardBody, Heading, Image, Text } from '@chakra-ui/react';
-import PlatformIconList from './PlatformIconList';
+import React from "react";
+import { Game } from "../hooks/useGames";
+import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
-interface Props{
-    game:Game;
+interface Props {
+  game: Game;
 }
-const GameCard = ({game}:Props) => {
+const GameCard = ({ game }: Props) => {
   return (
-   <Card borderRadius={10} overflow='hidden'>
-        <Image src={game.background_image}></Image>
-        <CardBody>
-            <Heading fontFamily='2xl'>{game.name}</Heading>
-            <PlatformIconList platforms={game.parent_platforms.map(p=> p.platform)}></PlatformIconList>
-        </CardBody>
-   </Card>
-  )
-}
+    <Card borderRadius={10} overflow="hidden">
+      <Image src={game.background_image}></Image>
+      <CardBody>
+        <Heading fontFamily="2xl">{game.name}</Heading>
+        <HStack justifyContent={"space-between"}>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic}></CriticScore>
+        </HStack>
+      </CardBody>
+    </Card>
+  );
+};
 
-export default GameCard
+export default GameCard;
