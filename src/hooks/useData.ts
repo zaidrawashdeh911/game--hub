3fb5e6adcfbe27ae8ec029545b2aa9d3d034dc05
zaidrawashdeh911 <zai20200907@std.psut.edu.jo>
2025-03-23 00:08:13 +0300

    Create a generic data fetching hook

---
+++ src/hooks/useData.ts
@@ -0,0 +1,37 @@
+import { CanceledError } from "axios";
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+
+interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
+const useData = <T>(endpoint: string) => {
+  const [data, setData] = useState<T[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  //to set a fetch request to the backend
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setLoading(true);
+    apiClient
+      .get<FetchResponse<T>>(endpoint, { signal: controller.signal })
+      .then((res) => {
+        setData(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+    setLoading(false);
+    return () => controller.abort();
+  }, []); // the empty array this, is the array of dependencies, without it we can't send a request to the backend
+
+  return { data, error, isLoading };
+};
+
+export default useData;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,48 +1,18 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
-export interface Platform{
-    id: number;
-    name: string;
-    slug: string;
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
 }
-export interface Game{
-    id:number;
-    name:string;
-    background_image: string;
-    parent_platforms: {platform: Platform} [];
-    metacritic: number;
+export interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 
-interface FetchGamesResponse{
-    count: number;
-    results: Game[]
-}
-
-const useGames = ()=>{
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState('');
-    const [isLoading, setLoading] = useState(false);
-
-    //to set a fetch request to the backend
-    useEffect(()=>{
-        const controller = new AbortController();
-
-        setLoading(true);
-        apiClient.get<FetchGamesResponse>('/games', {signal: controller.signal})
-        .then(res=> {
-            setGames(res.data.results);
-            setLoading(false);
-        })
-        .catch(err=>{
-            if(err instanceof CanceledError) return;
-            setError(err.message)});
-            setLoading(false);
-        return ()=> controller.abort();
-    }, []);// the empty array this, is the array of dependencies, without it we can't send a request to the backend
-
-    return {games, error, isLoading};
-}
+const useGames = () => useData<Game>("/games");
 
 export default useGames;
